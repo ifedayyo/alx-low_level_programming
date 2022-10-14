c69fa16e175a236867e96689f990dc75face3dd7
ifedayyo <ifedayyo12@gmail.com>
2022-10-14 14:49:08 -0700

    c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
 */
 
 int main(void)
 {
-	char c; 
-	c = 'z';
+	char c;
 
+	int d;
+
+	c = 'a';
+	d = 0;
 	while
-		(c >= 'a') {
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
 			putchar(c);
-			c--;
+			c++;
 		}
 	putchar('\n');
 	return (0);
