601d1ffad7ca5d355ce8afa6be1e769bd6f598bf
ifedayyo <ifedayyo12@gmail.com>
2022-10-18 13:43:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabets in lower case
+ * @letter
+ * Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
+
