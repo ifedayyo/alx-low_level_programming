670e035c01579e549af48696ed87b9b2ddf4d6cd
ifedayyo <ifedayyo12@gmail.com>
2022-10-20 15:18:47 -0700

    t

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,28 +2,29 @@
 
 /**
  * print_triangle - prints a triangle
- * @n: size of the triangle
+ * @size: size of the triangle
+ * Return: 0
 */
 
-void print_triangle(int n)
+void print_triangle(int size)
 {
-	int h, tri;
+	int i = 0, j, n = size - 1;
 
-	if (n > 0)
+	if (size > 0)
 	{
-		for (h = 1; h <= n; h++)
+		for (; i < size; i++)
 		{
-			for (tri = n - h; tri > 0; tri--)
-				_putchar(' ');
-			for (tri = 0; tri < h; tri++)
-				_putchar('#');
-
-			if (h == n)
-				continue;
-
-			_putcahr('\n');
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
 		}
 	}
-
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
