fbf2c64b37cd5c022b2af44eeb6a5f5426689f0b
ifedayyo <ifedayyo12@gmail.com>
2022-10-27 21:12:31 -0700

    t

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,7 +8,8 @@
  * @size_r: buffer size
  *
  * Return: address of r or 0
- */char *infinite_add(char *n1, char *n2, char *r, int size_r)
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 {
 	int i, j, k, l, m, n;
